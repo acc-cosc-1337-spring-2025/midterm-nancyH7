15145699fdc54ba7ffb02aced8213d5ce9951cbc
nancyH7 <nancygiselle.hernandez@g.austincc.edu>
2025-03-16 04:35:03 +0000

    question 1 - dna string

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,34 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
 
-int main()
-{
+double get_dna_p_distance(const string& dna1, const string& dna2){
+    if (dna1.length() != dna2.length()){
+        cerr << "Error: DNA sequence muct be of equal length!" << endl;
+        return -1;
+    }
+    int differences = 0;
+    int length = dna1.length();
+    for (int i=0; i < length; ++i){
+        if (dna1[i] != dna2[i]){
+            ++differences;
+        }
+        
+    }
+    return static_cast<double>(differences)/length;
 
+}
+
+int main(){
+    const string dna1 = "GAGCCTACTAACGGGAT";
+    const string dna2 = "CATCGTAATGACGGCCT";
+
+    double p_distance = get_dna_p_distance(dna1, dna2);
+
+    if (p_distance != -1){
+        cout << "P-Distance: " << fixed << setprecision(4) << endl;
+
+    }
     return 0;
 }
